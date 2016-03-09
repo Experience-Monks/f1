ba9e2345553df15e7da7a34740a1cd52b44c1355
Mikko Haapoja <me@mikkoh.com>
2016-03-09 16:45:57 -0500

    Documented the chief example and edited the documentation for the f1 instance

--- example/chief/index.js
+++ example/chief/index.js
@@ -1,23 +1,33 @@
 var chief = require('../../chief');
 var button = require('./button');
 
+// we'll use this variable to keep track of the button we've selected previously
 var selectedButton = null;
+
+// the button function returns an object which will have two variables
+// ui - an f1 instance
+// el - a dom element we should append to the dom
 var button1 = button({ text: 'Toronto', zIndex: 4 });
 var button2 = button({ text: 'Helsinki', zIndex: 3 });
 var button3 = button({ text: 'Vancouver', zIndex: 2 });
 var button4 = button({ text: 'Seinäjoki', zIndex: 1 });
 
+// the following events will add mouse events to all buttons
 addMouseEvents(button1, 1);
 addMouseEvents(button2, 2);
 addMouseEvents(button3, 3);
 addMouseEvents(button4, 4);
 
+// add the elements to the body
 document.body.appendChild(button1.el);
 document.body.appendChild(button2.el);
 document.body.appendChild(button3.el);
 document.body.appendChild(button4.el);
 
-var page = chief({
+// this will create our chief instance
+var menu = chief({
+  // here we define what f1 instances
+  // this chief instance will target
   targets: {
     button1: button1.ui,
     button2: button2.ui,
@@ -25,7 +35,14 @@ var page = chief({
     button4: button4.ui
   },
 
+  // here we'll define the states of the chief instance
   states: {
+
+    // an out state
+    // each value willd define what state
+    // each ui instance should be in when we're in the
+    // out state eg button1 should be in "out" when this
+    // chief is in it's "out" state
     out: {
       button1: 'out',
       button2: 'out',
@@ -33,6 +50,7 @@ var page = chief({
       button4: 'out'
     },
 
+    // an idle state
     idle: {
       button1: 'idle',
       button2: 'idle',
@@ -40,6 +58,7 @@ var page = chief({
       button4: 'idle'
     },
 
+    // a state when button1 is selected
     selected1: {
       button1: 'selected',
       button2: 'idle',
@@ -47,6 +66,7 @@ var page = chief({
       button4: 'idle'
     },
 
+    // a state when button2 is selected
     selected2: {
       button1: 'idle',
       button2: 'selected',
@@ -54,6 +74,7 @@ var page = chief({
       button4: 'idle'
     },
 
+    // a state when button3 is selected
     selected3: {
       button1: 'idle',
       button2: 'idle',
@@ -61,6 +82,7 @@ var page = chief({
       button4: 'idle'
     },
 
+    // a state when button4 is selected
     selected4: {
       button1: 'idle',
       button2: 'idle',
@@ -69,7 +91,11 @@ var page = chief({
     }
   },
 
+  // this will define the transitions of this chief instance
+  // you cannot use durations or eases
   transitions: [
+    // for out to idle we'll want to stagger animate in
+    // the buttons
     { 
       from: 'out', to: 'idle', bi: true, animation: {
         button2: { delay: 0.3 },
@@ -77,6 +103,9 @@ var page = chief({
         button4: { delay: 0.9 }
       }
     },
+    // to go from the selected states we'll want to go
+    // back to the idle state and then go to the selected 
+    // state
     { from: 'idle', to: 'selected1', bi: true },
     { from: 'idle', to: 'selected2', bi: true },
     { from: 'idle', to: 'selected3', bi: true },
@@ -84,12 +113,18 @@ var page = chief({
   ]
 });
 
-page.init('out');
-page.go('idle', function() {
+// initialize the chief instance
+menu.init('out');
+
+// animate the chief instance to the idle state
+menu.go('idle', function() {
   console.log('is in idle');
 });
 
 function addMouseEvents(button, buttonNum) {
+
+  // when we mouse over and out we'll animate individual
+  // buttons if they are not selected
   button.el.addEventListener('mouseover', function() {
     if(selectedButton !== buttonNum) {
       button.ui.go('over');  
@@ -102,6 +137,8 @@ function addMouseEvents(button, buttonNum) {
     }
   });
 
+  // when a button is selected we want to tell the menu
+  // to animate to the proper selected state
   button.el.addEventListener('click', function() {
     selectedButton = buttonNum;
     page.go('selected' + buttonNum, function() {
--- example/f1/index.js
+++ example/f1/index.js
@@ -21,17 +21,24 @@ var ui = f1({
   parsers: require('./parsers')
 });
 
+// this will initialize the ui instance in a state
+// it will cause the parsers update functions to be run for the first time
 ui.init('out');
+
+// this will tell the ui to animate to the idle state
 ui.go('idle');
 
+// on mouse over we'll want to animate to the over state
 itemToAnimate.addEventListener('mouseover', function() {
   ui.go('over');
 });
 
+// on mouse out we'll want to animate back to the idle state
 itemToAnimate.addEventListener('mouseout', function() {
   ui.go('idle');
 });
 
+// on mouse click we'll want to animate out the button
 itemToAnimate.addEventListener('click', function() {
   ui.go('out');
 });
